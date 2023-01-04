6bbe2b72fad6ea9c343a84af79d4806c4e8b2c7b
Anton <anton.vigstrand@gmail.com>
2023-01-04 17:10:04 +0100

    test: added test cases, including snapshot

--- src/pages/Layout/__test__/Layout.test.tsx
+++ src/pages/Layout/__test__/Layout.test.tsx
@@ -1,4 +1,5 @@
 import { /*Enzyme,*/ shallow } from "enzyme";
+import toJson from "enzyme-to-json";
 // import Adapter from "enzyme-adapter-react-16";
 import React from "react";
 import Layout from "../Layout";
@@ -8,5 +9,18 @@ import Layout from "../Layout";
 describe("Render Layout component", () => {
   it("renders without crashing", () => {
     shallow(<Layout />);
+  });
+
+  it("check for jsx elements", () => {
+    const wrapper = shallow(<Layout />);
+    const sidebar = <div className="sidebar"></div>;
+    // const value = wrapper.find("h2").text();
+    expect(wrapper.contains("Visualization")).toEqual(true);
+    // expect(value).toEqual("hej");
+  });
+
+  it("renders correctly", () => {
+    const tree = shallow(<Layout />);
+    expect(toJson(tree)).toMatchSnapshot();
   });
 });
