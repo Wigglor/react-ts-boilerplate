0a78ca45285dd51b599af85d204dc2538147bba9
Notna <anton.vigstrand@gmail.com>
2023-06-29 17:12:35 +0200

    feat: added some style to homepage

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Account from "./pages/Account/Account";
 import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
 import Unauthorized from "./pages/Unauthorized/Unauthorized";
+// import RequireAuth from "./components/requireAuth";
 
 const App = () => {
   // const [token, setToken] = useState<string | null>();
@@ -23,7 +24,16 @@ const App = () => {
           <Route path="login" element={<Login />} />
           <Route path="unauthorized" element={<Unauthorized />} />
 
-          {/* <Route element={<RequireAuth />}> */}
+          {/* <Route
+            element={
+              <>
+                <RequireAuth />
+
+                <NavBar />
+                <SideNav />
+              </>
+            }
+          > */}
           <Route
             element={
               <>
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,24 +1,11 @@
 import { ReactElement } from "react";
-import "./Home.module.scss";
-
-// async function http(request: RequestInfo): Promise<any> {
-//   const response = await fetch(request);
-//   const body = await response.json();
-//   return body;
-// }
-
-// interface Todo {
-//   userId: number;
-//   id: number;
-//   title: string;
-//   completed: boolean;
-// }
+import styles from "./Home.module.scss";
 
 const Home = (): ReactElement => {
   return (
-    <div>
-      <h1>Home Page</h1>
-    </div>
+    <main className={styles.Home}>
+      <h1>Home Page </h1>
+    </main>
   );
 };
 
