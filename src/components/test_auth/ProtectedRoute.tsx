bf4066005a1b72e7bfcd88b05484082cd22617e1
Notna <anton.vigstrand@gmail.com>
2023-07-03 13:01:28 +0200

    feat: added protected route

---
+++ src/components/test_auth/ProtectedRoute.tsx
@@ -0,0 +1,16 @@
+// src/ProtectedRoute.tsx
+
+import React from "react";
+import { Navigate, Outlet, RouteProps } from "react-router-dom";
+import { useAuth } from "../../context/test_auth/AuthContext";
+
+const ProtectedRoute: React.FC<RouteProps> = () => {
+  const { user } = useAuth()!;
+
+  //   return (
+  //     <Route {...rest} render={({ location }) => user ? (children) : (<Navigate to="/unauthorized" state={{ from: location }} replace />)} />
+  //   );
+  return user ? <Outlet /> : <Navigate to="/unauthorized" state={{ from: location }} replace />;
+};
+
+export default ProtectedRoute;
