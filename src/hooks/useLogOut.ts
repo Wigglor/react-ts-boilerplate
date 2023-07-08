85c59bf2d065252ac8e3a54cb3b20745d0dbd40d
Notna <anton.vigstrand@gmail.com>
2023-07-08 13:45:02 +0200

    feat: added logout hook

---
+++ src/hooks/useLogOut.ts
@@ -0,0 +1,26 @@
+import axios from "../api/axios";
+import useAuth from "./useAuth";
+
+const useLogout = () => {
+  const { setAuth } = useAuth();
+
+  const logout = async () => {
+    setAuth({
+      user: "",
+      // pwd: pwd,
+      accessToken: "",
+    });
+    try {
+      // const response = await axios('/cognito/signout', {
+      await axios("/cognito/signout", {
+        withCredentials: true,
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return logout;
+};
+
+export default useLogout;
