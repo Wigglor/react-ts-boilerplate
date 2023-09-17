8e1f3b92e52f2f62252552b3f5245d7409ca9839
Notna <anton.vigstrand@gmail.com>
2023-09-17 21:42:37 +0200

    feat: added type tp stripe options

--- src/pages/Billing/Billing2.tsx
+++ src/pages/Billing/Billing2.tsx
@@ -1,10 +1,10 @@
 import { ReactElement, useState } from "react";
 // import { CardElement, Elements, useElements, useStripe } from "@stripe/react-stripe-js";
 import { Elements, PaymentElement, useElements, useStripe } from "@stripe/react-stripe-js";
-import { loadStripe } from "@stripe/stripe-js";
+import { StripeElementsOptions, loadStripe } from "@stripe/stripe-js";
 // import axiosPrivate from "../../api/axios";
+import { redirect } from "react-router-dom";
 import useAxiosPrivate from "../../hooks/useAxiosPrivate";
-// import { redirect } from "react-router-dom";
 import styles from "./Billing.module.scss";
 
 // const stripePromise = loadStripe(process.env.STRIPE_SECRET_KEY as string);
@@ -13,9 +13,56 @@ const stripePromise = loadStripe(
 );
 
 const Billing = (): ReactElement => {
-  const options = {
+  // const options = {
+  //   // passing the client secret obtained in step 3
+  //   clientSecret: "pi_3NqytnBs7XdR639727iAKt2t_secret_HAjaNHEvkbV9Dk0Ua3oS1cYhl ",
+  //   // Fully customizable with appearance API.
+
+  //   appearance: {
+  //     // "stripe" | "night" | "flat"
+  //     theme: "flat" as const,
+  //     // variables: {
+  //     //   colorPrimary: "#0570de",
+  //     //   colorBackground: "#ffffff",
+  //     //   colorText: "#30313d",
+  //     //   colorDanger: "#df1b41",
+  //     //   fontFamily: "Ideal Sans, system-ui, sans-serif",
+  //     //   spacingUnit: "2px",
+  //     //   borderRadius: "4px",
+  //     //   // See all possible variables below
+  //     // },
+  //   },
+  // };
+  console.log(`app url: ${window.location.origin}`);
+
+  return (
+    <main className={styles.Billing}>
+      <div className={styles.stripe_element}>
+        {/* <Elements stripe={stripePromise} options={options}> */}
+        <CheckoutForm />
+        {/* </Elements> */}
+      </div>
+    </main>
+  );
+};
+
+const CheckoutForm = () => {
+  const stripe = useStripe();
+  const elements = useElements();
+  const axiosPrivate = useAxiosPrivate();
+
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+
+  type StripeOptions = {
+    clientSecret: string | null;
+    appearance: {
+      theme: string;
+    };
+  };
+
+  const options: StripeElementsOptions = {
     // passing the client secret obtained in step 3
-    clientSecret: "pi_3NqytnBs7XdR639727iAKt2t_secret_HAjaNHEvkbV9Dk0Ua3oS1cYhl ",
+    clientSecret: undefined,
     // Fully customizable with appearance API.
 
     appearance: {
@@ -33,46 +80,30 @@ const Billing = (): ReactElement => {
       // },
     },
   };
-  console.log(`app url: ${window.location.origin}`);
-
-  return (
-    <main className={styles.Billing}>
-      <div className={styles.stripe_element}>
-        <Elements stripe={stripePromise} options={options}>
-          <CheckoutForm />
-        </Elements>
-      </div>
-    </main>
-  );
-};
-
-const CheckoutForm = () => {
-  const stripe = useStripe();
-  const elements = useElements();
-  const axiosPrivate = useAxiosPrivate();
-
-  const [errorMessage, setErrorMessage] = useState<string | null>(null);
 
   const handleSubmit = async (event: React.FormEvent) => {
     // We don't want to let default form submission happen here,
     // which would refresh the page.
     event.preventDefault();
+
     try {
       const response = await axiosPrivate.post(
         "/billing",
-        JSON.stringify({ price: "price_1NlvxsBs7XdR63976skckphI" }),
+        // JSON.stringify({ price: "price_1NlvxsBs7XdR63976skckphI" }),
         {
           headers: { "Content-Type": "application/json" },
           withCredentials: true,
         },
       );
       console.log(`billing response: ${JSON.stringify(response)}`);
+      const clientSecret = response.data.paymentIntent.client_secret;
+      options.clientSecret = clientSecret;
     } catch (err: any) {
       const errorMessage = err?.response.data.message;
       setErrorMessage(errorMessage);
     }
 
-    /*if (!stripe || !elements) {
+    if (!stripe || !elements) {
       // Stripe.js hasn't yet loaded.
       // Make sure to disable form submission until Stripe.js has loaded.
       return;
@@ -98,19 +129,21 @@ const CheckoutForm = () => {
       // Your customer will be redirected to your `return_url`. For some payment
       // methods like iDEAL, your customer will be redirected to an intermediate
       // site first to authorize the payment, then redirected to the `return_url`.
-    }*/
+    }
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <PaymentElement />
-      {/* Payment succeeds - regular card: 4242424242424242 */}
-      {/* Payment requires authentication - SCA card: 4000002500003155 */}
-      {/* Payment is declined: 4000000000009995 */}
-      <button disabled={!stripe}>Submit</button>
-      {/* Show error message to your customers */}
-      {errorMessage && <div>{errorMessage}</div>}
-    </form>
+    <Elements stripe={stripePromise} options={options}>
+      <form onSubmit={handleSubmit}>
+        <PaymentElement />
+        {/* Payment succeeds - regular card: 4242424242424242 */}
+        {/* Payment requires authentication - SCA card: 4000002500003155 */}
+        {/* Payment is declined: 4000000000009995 */}
+        <button disabled={!stripe}>Submit</button>
+        {/* Show error message to your customers */}
+        {errorMessage && <div>{errorMessage}</div>}
+      </form>
+    </Elements>
   );
 };
 
