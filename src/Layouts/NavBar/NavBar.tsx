08ec11981938833a913dde87c1ecb497f08ba2e5
Notna <anton.vigstrand@gmail.com>
2024-01-16 21:37:37 +0100

    style: changed navbar layout

--- src/Layouts/NavBar/NavBar.tsx
+++ src/Layouts/NavBar/NavBar.tsx
@@ -807,24 +807,163 @@ const NavBar = (): ReactElement => {
         </div>
       </nav> */}
 
-      <header className="bg-gray-50 z-10 border-b p-2 flex-no-wrap relative flex w-full items-center">
-        <nav className="flex w-full flex-wrap items-center justify-between px-3">
+      <header className="bg-gray-50 z-10 border-b flex-no-wrap relative flex w-full items-center">
+        <div className="bg-slate-500 p-2">
           <div className="flex">
             <Link to="/">
               <Home />
             </Link>
           </div>
-          {/* <ul className="flex">
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/contact">Contact</Link>
-            </li>
-          </ul> */}
+        </div>
+        <div className="flex w-full flex-wrap items-center p-2 justify-between">
+          <div className="relative inline-block text-left">
+            <div>
+              <button
+                type="button"
+                className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+                id="menu-button"
+                aria-expanded="true"
+                aria-haspopup="true"
+              >
+                Options
+                <svg
+                  className="-mr-1 h-5 w-5 text-gray-400"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </button>
+            </div>
+
+            <div
+              className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+              role="menu"
+              aria-orientation="vertical"
+              aria-labelledby="menu-button"
+              tabIndex={-1}
+            >
+              <div className="" role="none">
+                <a
+                  href="#"
+                  className="text-gray-700 block px-4 text-sm"
+                  role="menuitem"
+                  tabIndex={-1}
+                  id="menu-item-0"
+                >
+                  Account settings
+                </a>
+                <a
+                  href="#"
+                  className="text-gray-700 block px-4  text-sm"
+                  role="menuitem"
+                  tabIndex={-1}
+                  id="menu-item-1"
+                >
+                  Support
+                </a>
+                <a
+                  href="#"
+                  className="text-gray-700 block px-4  text-sm"
+                  role="menuitem"
+                  tabIndex={-1}
+                  id="menu-item-2"
+                >
+                  License
+                </a>
+                <form method="POST" action="#" role="none">
+                  <button
+                    type="submit"
+                    className="text-gray-700 block w-full px-4  text-left text-sm"
+                    role="menuitem"
+                    tabIndex={-1}
+                    id="menu-item-3"
+                  >
+                    Sign out
+                  </button>
+                </form>
+              </div>
+            </div>
+          </div>
+
+          {/* {workSpaces.selectedWorkSpace.id.length > 0 && (
+            <div>
+              <select
+                className="rounded-lg cursor-pointer"
+                value={stateSelectedWorkspace}
+                onChange={handleChange}
+              >
+                {workSpaces.selectedWorkSpace ? (
+                  workSpaces.availableWorkSpaces.map((item: Workspace) => (
+                    <option key={item.id} value={item.name}>
+                      {item.name}
+                    </option>
+                  ))
+                ) : (
+                  <></>
+                )}
+              </select>
+            </div>
+          )} */}
+
+          <div className="relative group">
+            <CircleUserRound className="cursor-pointer" />
+            <ul className="hidden absolute group-hover:block transition duration-2000">
+              <li className="">
+                <p>signed in as</p>
+                <p>
+                  <b>Testuser</b>
+                </p>
+              </li>
+              <li className="">
+                <Link to="/organization">Organization</Link>
+              </li>
+              <li className="">
+                <Link to="/account">Account Settings</Link>
+              </li>
+              <li className="">
+                <Link to="/login" onClick={signOut}>
+                  Logout
+                </Link>
+              </li>
+            </ul>
+          </div>
+
+          <div className="group">
+            <CircleUserRound className="cursor-pointer" />
+            <ul className="bg-white shadow-lg rounded-md border absolute hidden group-hover:block transition-all duration-500 ease right-2">
+              <li className="px-2 ">
+                <p>signed in as</p>
+                <p>
+                  <b>Testuser</b>
+                </p>
+              </li>
+              <li className="px-2">
+                <Link to="/organization">Organization</Link>
+              </li>
+              <li className="px-2">
+                <Link to="/account">Account Settings</Link>
+              </li>
+              <li className="px-2">
+                <Link to="/login" onClick={signOut}>
+                  Logout
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+        {/* <nav className="flex w-full flex-wrap items-center justify-between">
+          <div className="flex bg-black">
+            <Link to="/">
+              <Home />
+            </Link>
+          </div>
+         
           <div className="relative inline-block text-left">
             <div>
               <button
@@ -902,8 +1041,6 @@ const NavBar = (): ReactElement => {
 
           {workSpaces.selectedWorkSpace.id.length > 0 && (
             <div>
-              {/* <label>
-                workspace */}
               <select
                 className="rounded-lg cursor-pointer"
                 value={stateSelectedWorkspace}
@@ -919,7 +1056,6 @@ const NavBar = (): ReactElement => {
                   <></>
                 )}
               </select>
-              {/* </label> */}
             </div>
           )}
 
@@ -947,9 +1083,7 @@ const NavBar = (): ReactElement => {
           </div>
 
           <div className="group">
-            {/* <Link to="/account">
-              <MdManageAccounts />
-            </Link> */}
+            
             <CircleUserRound className="cursor-pointer" />
             <ul className="bg-white shadow-lg p-2 rounded-md border absolute hidden group-hover:block transition-all duration-500 ease right-2">
               <li className="px-2 py-1">
@@ -971,7 +1105,7 @@ const NavBar = (): ReactElement => {
               </li>
             </ul>
           </div>
-        </nav>
+        </nav> */}
       </header>
       <Outlet />
     </>
