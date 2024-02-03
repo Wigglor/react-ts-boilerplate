96dc559fadef878d619bd3d6f1b597b36a1b7363
Notna <anton.vigstrand@gmail.com>
2024-02-04 00:18:50 +0100

    feat: added cancel button to modal

--- src/pages/Organization/Organization.tsx
+++ src/pages/Organization/Organization.tsx
@@ -284,14 +284,23 @@ const Organization = ({ allowedRoles }: RequireAuthProps): ReactElement => {
                       </div>
                     </div>
                   </div>
-
-                  <button
-                    type="button"
-                    onClick={() => handleDeleteClick(deleteEmail)}
-                    className="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
-                  >
-                    Delete User
-                  </button>
+                  <div className="flex justify-end items-center gap-x-2 py-3 px-4 bg-gray-50 border-t dark:bg-gray-800 dark:border-gray-700">
+                    <button
+                      onClick={closeDeleteModal}
+                      type="button"
+                      className="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+                      data-hs-overlay="#hs-notifications"
+                    >
+                      Cancel
+                    </button>
+                    <button
+                      type="button"
+                      onClick={() => handleDeleteClick(deleteEmail)}
+                      className="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+                    >
+                      Delete User
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
