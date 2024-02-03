9189a75c9f40011c37a93691e76ed0dfd6a45a38
Notna <anton.vigstrand@gmail.com>
2024-02-03 17:28:48 +0100

    fix: restyled dele user modal

--- src/pages/Organization/Organization.tsx
+++ src/pages/Organization/Organization.tsx
@@ -228,7 +228,68 @@ const Organization = (): ReactElement => {
           className="fixed top-0 left-0 w-full h-full bg-gray-300 bg-opacity-80 z-40 flex justify-center items-center"
           onClick={closeDeleteModal}
         >
-          <div className="bg-gray-100 text-black p-2" onClick={(e) => e.stopPropagation()}>
+          <div
+            className="mt-7 bg-white border border-gray-200 rounded-xl shadow-sm dark:bg-gray-800 dark:border-gray-700"
+            onClick={(e) => e.stopPropagation()}
+          >
+            <div className="p-4 sm:p-7">
+              <div className="text-center">
+                {/* <h1 className="block text-2xl font-bold text-gray-800 dark:text-white">
+                  Forgot password?
+                </h1> */}
+                <p className="mt-2 text-sm text-gray-600 dark:text-gray-400">
+                  Please type in the email: <i>{deleteEmail} to delete the user</i>
+                </p>
+              </div>
+
+              <div className="mt-5">
+                {deleteUserErrorMessage && (
+                  <p className="bg-red-600 p-3">{deleteUserErrorMessage}</p>
+                )}
+                <div className="grid gap-y-4">
+                  <div>
+                    <label htmlFor="email" className="block text-sm mb-2 dark:text-white">
+                      Email address
+                    </label>
+                    <div className="relative">
+                      <input
+                        id="email"
+                        type="email"
+                        value={emailCheckValue}
+                        onChange={(e) => handleEmailChange(e, "setEmailCheckValue")}
+                        name="email"
+                        className="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600"
+                        required
+                        aria-describedby="email-error"
+                      ></input>
+                      <div className="hidden absolute inset-y-0 end-0 pointer-events-none pe-3">
+                        <svg
+                          className="h-5 w-5 text-red-500"
+                          width="16"
+                          height="16"
+                          fill="currentColor"
+                          viewBox="0 0 16 16"
+                          aria-hidden="true"
+                        >
+                          <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
+                        </svg>
+                      </div>
+                    </div>
+                  </div>
+
+                  <button
+                    type="button"
+                    onClick={() => handleDeleteClick(deleteEmail)}
+                    className="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+                  >
+                    Delete User
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {/* <div className="bg-gray-100 text-black p-2" onClick={(e) => e.stopPropagation()}>
             {deleteUserErrorMessage && <p className="bg-red-600 p-3">{deleteUserErrorMessage}</p>}
             <p>
               Please type in the email: <i>{deleteEmail} to delete the user</i>
@@ -242,7 +303,7 @@ const Organization = (): ReactElement => {
             <button type="button" onClick={() => handleDeleteClick(deleteEmail)}>
               Delete User
             </button>
-          </div>
+          </div> */}
         </div>
       )}
       {inviteEmail && (
@@ -358,7 +419,6 @@ const Organization = (): ReactElement => {
           </div>
         </div>
       )}
-      {/* <div className="bg-gray-200 p-4"> */}
       <div>
         <div className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
           <div className="flex flex-col">
@@ -598,7 +658,6 @@ const Organization = (): ReactElement => {
           </div>
         </div>
       </div>
-      {/* </div> */}
     </main>
   );
 };
