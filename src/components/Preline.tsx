ac50b16b81b6ac58ff0e5f0d565b2c5f1ac0dd49
Notna <anton.vigstrand@gmail.com>
2024-02-15 19:50:09 +0100

    feat: preline file

--- src/components/Preline.tsx
+++ src/components/Preline.tsx
@@ -1,6 +1,12 @@
-/*import { useEffect } from 'react';
-
-import { useLocation } from 'react-router-dom';
+import { HSDropdown, IStaticMethods } from "preline/preline";
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+declare global {
+  interface Window {
+    HSStaticMethods: IStaticMethods;
+    DDStaticMethods: HSDropdown;
+  }
+}
 
 const Preline = () => {
   const location = useLocation();
@@ -10,11 +16,11 @@ const Preline = () => {
   }, []);
 
   useEffect(() => {
-    // @ts-ignore
-    HSStaticMethods.autoInit();
+    window.HSStaticMethods.autoInit();
+    // HSStaticMethods.autoInit();
   }, [location.pathname]);
 
   return <></>;
 };
 
-export default Preline;*/
+export default Preline;
