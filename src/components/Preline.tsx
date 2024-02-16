06ca277abdb5d47d1ab2df5f553a73c3ed2c9d30
Notna <anton.vigstrand@gmail.com>
2024-02-16 10:16:12 +0100

    fix: preline comp

--- src/components/Preline.tsx
+++ src/components/Preline.tsx
@@ -1,27 +1,23 @@
-import { useEffect } from "react";
-
-import { useLocation } from "react-router-dom";
-
 // import "preline/preline";
-import { HSDropdown, IStaticMethods } from "preline/preline";
-declare global {
-  interface Window {
-    HSStaticMethods: IStaticMethods;
-    DDStaticMethods: HSDropdown;
-  }
-}
+// import { HSDropdown, IStaticMethods } from "preline/preline";
+// declare global {
+//   interface Window {
+//     HSStaticMethods: IStaticMethods;
+//     DDStaticMethods: HSDropdown;
+//   }
+// }
 
 const Preline = () => {
-  const location = useLocation();
+  // const location = useLocation();
 
-  useEffect(() => {
-    import("preline/preline");
-  }, []);
+  // useEffect(() => {
+  //   import("preline/preline");
+  // }, []);
 
-  useEffect(() => {
-    // @ts-ignore
-    HSStaticMethods.autoInit();
-  }, [location.pathname]);
+  // useEffect(() => {
+
+  //   HSStaticMethods.autoInit();
+  // }, [location.pathname]);
 
   return <></>;
 };
