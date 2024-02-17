ed4548947e662cca60c257136608be6017a6a886
Notna <anton.vigstrand@gmail.com>
2024-02-17 16:35:38 +0100

    style: trying to add full height

--- src/Layouts/Navigation/Navigation.tsx
+++ src/Layouts/Navigation/Navigation.tsx
@@ -117,6 +117,8 @@ const Navigation = (): ReactElement => {
 
   return (
     <>
+      {/* <NavBar />
+      <SideNav /> */}
       <>
         <main className="h-full">
           <div className="flex h-full">
@@ -124,11 +126,11 @@ const Navigation = (): ReactElement => {
               // <nav
               // className={`flex flex-col content-center transition-width duration-300 ${
               // className={`flex flex-col content-center transition-width duration-300 hs-overlay hs-overlay-open:translate-x-0 -translate-x-full transition-all transform fixed top-0 start-0 bottom-0 z-[60] w-64 bg-white border-e border-gray-200 pt-7 pb-10 overflow-y-auto lg:block lg:translate-x-0 lg:end-auto lg:bottom-0 [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-track]:bg-gray-100 [&::-webkit-scrollbar-thumb]:bg-gray-300 dark:[&::-webkit-scrollbar-track]:bg-slate-700 dark:[&::-webkit-scrollbar-thumb]:bg-slate-500 dark:bg-gray-800 dark:border-gray-700 ${
-              className={`flex flex-col content-center transition-width duration-300 bg-white border-e border-gray-200  overflow-y-auto dark:bg-gray-800 dark:border-gray-700 ${
+              className={`flex h-full flex-col content-center transition-width bg-slate-300 duration-300  border-e border-gray-200  overflow-y-auto dark:bg-gray-800 dark:border-gray-700 ${
                 isCollapsed ? "w-16" : "w-1/12"
               }`}
             >
-              <nav className="">
+              <nav className="h-full ">
                 <div className="bg-gray-50 flex p-4 flex-col justify-center items-center">
                   <div className="">
                     <Link to="/">
@@ -157,7 +159,6 @@ const Navigation = (): ReactElement => {
               </nav>
             </div>
             <div
-              // className={`transition-width duration-300 ${
               className={`transition-width duration-300 text-sm"> ${
                 isCollapsed ? "w-[calc(100%-4rem)]" : "w-11/12"
               }`}
@@ -178,7 +179,7 @@ const Navigation = (): ReactElement => {
                         //   "toggleClasses": "hs-select-disabled:pointer-events-none hs-select-disabled:opacity-50 relative py-2 ps-3 pe-9 flex gap-x-2 text-nowrap w-full cursor-pointer bg-white border border-gray-200 rounded-lg text-start text-sm focus:outline-none focus:ring-2 focus:ring-blue-500 dark:bg-neutral-800 dark:border-neutral-700 dark:text-neutral-400 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-neutral-600",
                         //   "dropdownClasses": "mt-2 z-50 w-full max-h-[300px] p-1 space-y-0.5 overflow-hidden overflow-y-auto bg-white rounded-xl shadow-[0_10px_40px_10px_rgba(0,0,0,0.08)] [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-track]:bg-gray-100 [&::-webkit-scrollbar-thumb]:bg-gray-300 dark:[&::-webkit-scrollbar-track]:bg-neutral-700 dark:[&::-webkit-scrollbar-thumb]:bg-neutral-500 dark:bg-neutral-900",
                         //   "optionClasses": "hs-selected:bg-gray-100 dark:hs-selected:bg-neutral-800 py-2 px-4 w-full text-sm text-gray-800 cursor-pointer hover:bg-gray-100 rounded-lg focus:outline-none focus:bg-gray-100 dark:text-neutral-300 dark:hover:bg-neutral-800 dark:focus:bg-neutral-800",
-                        //   "optionTemplate": "<div class=\"flex justify-between items-center w-full\"><span data-title></span><span class=\"hidden hs-selected:block\"><svg class=\"flex-shrink-0 w-3.5 h-3.5 text-gray-800 dark:text-neutral-200\" xmlns=\"http:.w3.org/2000/svg\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\" fill=\"none\" stroke=\"currentColor\" stroke-width=\"2\" stroke-linecap=\"round\" stroke-linejoin=\"round\"><polyline points=\"20 6 9 17 4 12\"/></svg></span></div>"
+                        //   "optionTemplate": "<div className=\"flex justify-between items-center w-full\"><span data-title></span><span className=\"hidden hs-selected:block\"><svg className=\"flex-shrink-0 w-3.5 h-3.5 text-gray-800 dark:text-neutral-200\" xmlns=\"http:.w3.org/2000/svg\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\" fill=\"none\" stroke=\"currentColor\" strokeWidth=\"2\" strokeLinecap=\"round\" strokeLinejoin=\"round\"><polyline points=\"20 6 9 17 4 12\"/></svg></span></div>"
                         // }'
                         // className="hidden"
                         className="px-4 pe-9 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600"
@@ -228,7 +229,7 @@ const Navigation = (): ReactElement => {
                               <span className="pl-2 text-slate-700">Account Settings</span>
                             </li>
                           </Link>
-                          <Link to="/login" className="cursor-pointer" onClick={closeDropdown}>
+                          <Link to="/login" className="cursor-pointer" onClick={signOut}>
                             <li className="px-3 py-1.5 text-slate-700 hover:bg-gray-100">
                               <LogOut width="18" height="18" className="inline" />
                               <span className="pl-2 text-slate-700">Logout</span>
