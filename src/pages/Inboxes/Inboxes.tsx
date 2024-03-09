c9866f2d863d90dfec49bc15fff5bba477428827
Notna <anton.vigstrand@gmail.com>
2024-03-09 13:44:12 +0100

    feat: added modal inbox -wip

--- src/pages/Inboxes/Inboxes.tsx
+++ src/pages/Inboxes/Inboxes.tsx
@@ -1,9 +1,112 @@
-import { ReactElement } from "react";
+import { ReactElement, useState } from "react";
+import { createPortal } from "react-dom";
 
-const Home = (): ReactElement => {
+const Inboxes = (): ReactElement => {
+  const [addInbox, setAddInbox] = useState<boolean>(false);
+
+  const toggleInbox = () => {
+    setAddInbox((prev) => !prev);
+  };
+
   return (
     <>
       <div className="py-3">
+        {addInbox && (
+          <>
+            {createPortal(
+              <div
+                className="fixed top-0 left-0 w-full h-full bg-gray-300 bg-opacity-80 z-40 flex justify-center items-center"
+                onClick={toggleInbox}
+              >
+                <div className="" onClick={(e) => e.stopPropagation()}>
+                  <div className=" bg-white border border-gray-200 rounded-xl shadow-sm dark:bg-gray-800 dark:border-gray-700">
+                    <div className="p-4 sm:p-7">
+                      {/* {errorMessage && (
+                        <p className="bg-red-60 p-3 text-white rounded-lg w-full">{errorMessage}</p>
+                      )} */}
+                      <div className="text-center">
+                        <h1 className="block text-2xl font-bold text-gray-800 dark:text-white">
+                          Invite User
+                        </h1>
+                        <p className="mt-2 text-sm text-gray-600 dark:text-gray-400">
+                          Provide the email you want to send an invite to
+                        </p>
+                      </div>
+
+                      <div className="mt-5">
+                        <form>
+                          {/* <form onSubmit={handleSubmit(onSubmit)}> */}
+                          <div className="grid gap-y-4">
+                            <div>
+                              <label htmlFor="email" className="block text-sm mb-2 dark:text-white">
+                                Email address
+                              </label>
+                              {/* <input
+                                type="email"
+                                id="email"
+                                className="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600"
+                                required
+                                aria-describedby="email-error"
+                                {...register("email", {
+                                  required: "required",
+                                  pattern: {
+                                    value: /\S+@\S+\.\S+/,
+                                    message: "Entered value does not match email format",
+                                  },
+                                })}
+                              ></input> */}
+
+                              {/* {errors.email && (
+                                <span className="bg-red-600 text-white p-2" role="alert">
+                                  {errors.email.message}
+                                </span>
+                              )} */}
+                              <div className="relative">
+                                <div className="hidden absolute inset-y-0 end-0 pointer-events-none pe-3">
+                                  <svg
+                                    className="h-5 w-5 text-red-500"
+                                    width="16"
+                                    height="16"
+                                    fill="currentColor"
+                                    viewBox="0 0 16 16"
+                                    aria-hidden="true"
+                                  >
+                                    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
+                                  </svg>
+                                </div>
+                              </div>
+                              <p className="hidden text-xs text-red-600 mt-2" id="email-error">
+                                Please include a valid email address so we can get back to you
+                              </p>
+                            </div>
+
+                            <div className="flex justify-end items-center gap-x-2 py-3 px-4 bg-gray-50 border-t dark:bg-gray-800 dark:border-gray-700">
+                              <button
+                                onClick={toggleInbox}
+                                type="button"
+                                className="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+                                data-hs-overlay="#hs-notifications"
+                              >
+                                Cancel
+                              </button>
+                              <button
+                                type="submit"
+                                className="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+                              >
+                                Invite User
+                              </button>
+                            </div>
+                          </div>
+                        </form>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>,
+              document.getElementById("overlay-root") as HTMLElement,
+            )}
+          </>
+        )}
         <div className="px-4 sm:px-7 mb-3">
           <h3 className="font-semibold text-gray-800 dark:text-neutral-200">My Inboxes</h3>
         </div>
@@ -13,6 +116,7 @@ const Home = (): ReactElement => {
             <a
               className="p-4 group flex flex-col bg-white border border-gray-200 rounded-xl focus:outline-none dark:bg-neutral-900 dark:border-neutral-700"
               href="#"
+              onClick={toggleInbox}
             >
               <div className="mb-4 flex flex-col justify-center items-center h-full">
                 <span className="flex justify-center items-center h-12 w-12 xl:h-16 xl:w-16 mx-auto border-2 border-dotted border-gray-300 text-gray-400 rounded-2xl dark:border-neutral-700 dark:text-neutral-500">
@@ -81,4 +185,4 @@ const Home = (): ReactElement => {
   );
 };
 
-export default Home;
+export default Inboxes;
