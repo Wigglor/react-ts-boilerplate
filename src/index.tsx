5bd093a0c2bafa82cbe23899f66e1562ed43ef42
Notna <anton.vigstrand@gmail.com>
2024-05-02 17:22:04 +0200

    feat: added store to index file

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,9 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 import * as ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 // import { store } from "./store/store";
+import { Provider } from "react-redux";
 import { AuthProvider } from "./context/AuthProvider";
+import { store } from "./store";
 // import { WorkSpacesProvider } from "./context/WorkSpacesProvider";
 
 // import Preline from "./components/Preline";
@@ -17,17 +19,19 @@ if (!rootElement) throw new Error("Failed to find the root element");
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      {/* <Preline /> */}
-      <GoogleOAuthProvider clientId="523632201518-emi1ioba9vvudkc2u8on28gph5cc2cu0.apps.googleusercontent.com">
-        <AuthProvider>
-          {/* <WorkSpacesProvider> */}
-          {/* <Preline /> */}
-          <App />
-          {/* </WorkSpacesProvider> */}
-        </AuthProvider>
-      </GoogleOAuthProvider>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        {/* <Preline /> */}
+        <GoogleOAuthProvider clientId="523632201518-emi1ioba9vvudkc2u8on28gph5cc2cu0.apps.googleusercontent.com">
+          <AuthProvider>
+            {/* <WorkSpacesProvider> */}
+            {/* <Preline /> */}
+            <App />
+            {/* </WorkSpacesProvider> */}
+          </AuthProvider>
+        </GoogleOAuthProvider>
+      </BrowserRouter>
+    </Provider>
     ,
   </React.StrictMode>,
 );
