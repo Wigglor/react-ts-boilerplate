35ae7e33492121d9cf9fba6bd3edb54466ebeb3c
Notna <anton.vigstrand@gmail.com>
2024-05-15 19:23:18 +0200

    style: fetch red

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -8,7 +8,7 @@ export default axios.create({
 
 export const axiosProd = axios.create({
   baseURL: "http://16.170.240.223:3000/",
-});
+}); // fetch
 
 export const axiosPrivate = axios.create({
   baseURL: BASE_URL,
