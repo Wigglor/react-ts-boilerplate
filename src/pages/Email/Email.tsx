aede904d6ad7007df3591201b450be229524178c
Notna <anton.vigstrand@gmail.com>
2024-06-03 06:56:03 +0200

    feat: added word count card

--- src/pages/Email/Email.tsx
+++ src/pages/Email/Email.tsx
@@ -64,7 +64,45 @@ const Email = (): ReactElement => {
   return (
     <>
       <div className="py-3">
-        <h1>Word Count: {wordCount}</h1>
+        <div className="grid grid-cols-2 lg:grid-cols-4 gap-2 md:gap-3 xl:gap-5">
+          <div className="relative overflow-hidden p-4 sm:p-5 bg-white border border-gray-200 rounded-xl shadow-sm before:absolute before:top-0 before:end-0 before:size-full before:bg-gradient-to-br before:from-purple-100 before:via-transparent before:blur-xl dark:bg-neutral-800 dark:border-neutral-700 dark:before:from-purple-800/30 dark:before:via-transparent">
+            <div className="relative z-10">
+              <div className="flex justify-between gap-x-3">
+                <span className="mb-3 inline-flex justify-center items-center size-8 md:size-10 rounded-lg bg-white text-gray-700 shadow dark:bg-neutral-800 dark:border-neutral-700 dark:text-neutral-400">
+                  <svg
+                    className="flex-shrink-0 size-4 md:size-5 text-purple-500"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  >
+                    <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
+                    <circle cx="9" cy="7" r="4" />
+                    <path d="M22 21v-2a4 4 0 0 0-3-3.87" />
+                    <path d="M16 3.13a4 4 0 0 1 0 7.75" />
+                  </svg>
+                </span>
+              </div>
+
+              <h2 className="text-sm md:text-base text-gray-800 dark:text-neutral-200">
+                Word Count
+              </h2>
+
+              <div className="grid sm:flex sm:justify-between sm:items-center gap-1 sm:gap-3">
+                <h3 className="text-lg md:text-2xl font-semibold text-gray-800 dark:text-neutral-200">
+                  {wordCount}
+                </h3>
+
+                <div className="flex items-center -space-x-2"></div>
+              </div>
+            </div>
+          </div>
+        </div>
         <div
           //   className="max-w-full break-words overflow-hidden"
           dangerouslySetInnerHTML={{ __html: emailContent }}
